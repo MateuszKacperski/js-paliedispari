3e76ef3198f85d5dfb7aa5494181abe729908b4b
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-09 15:29:46 +0100

    added es 2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ console.log('JS OK');
 
 const userWord = prompt("inserisci una parola", "anna");
 
+
+/**
+ * This function is created for reversing a string. Add a string as a parameter and get it reversed
+ * @param {string} word String to reverse 
+ * @returns {string} Reversed string
+ */
 function reverseWord(word){
 
     let reverse = "";
@@ -34,3 +40,54 @@ if(userWord === results){
 
 
 //Es 2 pari o dispari
+
+const evenOdd = prompt("Cosa scegli pari o dispari", "pari");
+
+const userNumber = parseInt(prompt("Scegli un numero da 1 a 5", "3"));
+
+/**
+ * This function is created for return a random number from 1 to 5
+ * @returns {number} Return a random number from 1 to 5
+ */
+function randomNumber(){
+    const num = Math.floor((Math.random() * 5) + 1);
+ 
+    return num;
+}
+
+const randomNum = parseInt(randomNumber());
+
+console.log(randomNum);
+
+const tot = randomNum + userNumber;
+
+console.log(tot);
+
+let result = "";
+
+
+/**
+ * This function is created for check if number is even 
+ * @param {*number} num add a number to check
+ * @returns {boolean} retourn a bollean value true for even and fales for odd
+ */
+function isEven(num){
+    if(num % 2 === 0){
+        result += "pari";
+    }else{
+        result += "dispari";
+    }
+
+    return result;
+}
+
+const oddEven = isEven(tot);
+
+console.log(result);
+
+
+if(evenOdd === oddEven){
+    console.log("Complinenti hai vinto");
+}else{
+    console.log("Mi dispiace hai perso");
+}
